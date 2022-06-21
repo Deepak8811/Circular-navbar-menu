028e52e65cde7fe67d093e1756375d5c7c5fce1e
Deepak <deepchaurasiya1997@gmail.com>
2022-06-22 03:50:37 +0530

    Final commit of this project

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 let toggle = document.querySelector(".toggle");
 let menu = document.querySelector(".menu");
-toggle.onclick = function () {};
+toggle.onclick = function () {
+    menu.classList.toggle('active')
+
+};
